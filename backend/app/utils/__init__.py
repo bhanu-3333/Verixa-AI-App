9f543fc516d152008416221c2e0fb8217ec600f1
rindhiya970 <rindhiya731@gmail.com>
2026-07-07 14:44:26 +0530

    feat: initialize utils package with response and logger imports

---
+++ backend/app/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utils package init
+"""
+
+from app.utils.response import success_response, error_response
+from app.utils.logger import logger
+
+__all__ = ["success_response", "error_response", "logger"]
